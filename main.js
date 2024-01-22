297c7545b7d1fc4e2fc3d4012af71a434e1641de
Benovshe Meherremova <benovshe@Benovshes-MacBook-Pro.local>
2024-01-22 17:47:08 +0400

    time added

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 let cardsContainer = document.querySelector(".cards");
 let cards = document.querySelectorAll(".card");
-let timer  = document.querySelector('.time')
+let timer = document.querySelector(".time");
+let reset = document.querySelector(".reset");
 var minute = 0;
-var second = 0
-function getDate(){
-  setTimeout(()=>{
-    second++
-    if(second === 60){
-      minute+=1
-      second=0
-      return getDate()
-    }
-  },1000)
-}
-let countdown = getDate()
-timer.innerHTML = countdown
+var second = 0;
+let endValue;
+
+let setTime = function () {
+  second++;
+  if (second === 60) {
+    second = 0;
+    minute++;
+  }
+  const newMinute = minute<10 ? `0${minute}` : minute;
+  const newSecond = second<10 ? `0${second}` : second;
+  endValue = `${newMinute}:${newSecond}`;
+  timer.innerHTML = endValue;
+};
+const intervalId = setInterval(() => {
+  setTime();
+}, 100);
+reset.addEventListener("click",()=>{
+  clearInterval(setTime)
+},2000)
+// console.log(intervalId);
 let images = [
   "./Img/card1.png",
   "./Img/card2.png",
@@ -30,17 +39,19 @@ let images = [
   "/Img/card6.png",
 ];
 
-
-function shuffleArray(array) {
+function randomImage(array) {
   let shuffledArray = [...array];
   for (let i = shuffledArray.length - 1; i > 0; i--) {
     const randomIndex = Math.floor(Math.random() * (i + 1));
-    [shuffledArray[i], shuffledArray[randomIndex]] = [shuffledArray[randomIndex], shuffledArray[i]];
+    [shuffledArray[i], shuffledArray[randomIndex]] = [
+      shuffledArray[randomIndex],
+      shuffledArray[i],
+    ];
   }
   return shuffledArray;
 }
 
-let shuffledImages = shuffleArray(images);
+let uniqueImage = randomImage(images);
 
 let rotateCard = [];
 cards.forEach((card, index) => {
@@ -50,17 +61,15 @@ cards.forEach((card, index) => {
 
   firstCard.className = "first__card";
   secondCard.className = "second__card";
-  image.src = shuffledImages[index];
+  image.src = uniqueImage[index];
   card.appendChild(firstCard);
   card.appendChild(secondCard);
   firstCard.appendChild(image);
   secondCard.innerHTML = "?";
   card.addEventListener("click", () => {
     flipCard(card);
-
   });
 });
-
 
 function flipCard(clickedCard) {
   if (rotateCard.length < 2) {
@@ -69,7 +78,8 @@ function flipCard(clickedCard) {
 
     if (rotateCard.length === 2) {
       const backImageSrc = rotateCard[0].querySelector(".first__card img").src;
-      const backSecondImageSrc = rotateCard[1].querySelector(".first__card img").src;
+      const backSecondImageSrc =
+        rotateCard[1].querySelector(".first__card img").src;
 
       if (backImageSrc === backSecondImageSrc) {
         console.log(true);
@@ -80,11 +90,11 @@ function flipCard(clickedCard) {
             card.querySelector(".second__card").style.zIndex = "-1";
 
             card.querySelector(".second__card").style.opacity = "0";
-            card.querySelector('.first__card').style.transform = 'rotateY(0deg)';
-            card.querySelector('.first__card').style.visibility = 'inherit';
-            card.querySelector('.first__card').style.opacity = '0.7';
-            card.querySelector('.first__card').style.transition = '1s';
-
+            card.querySelector(".first__card").style.transform =
+              "rotateY(0deg)";
+            card.querySelector(".first__card").style.visibility = "inherit";
+            card.querySelector(".first__card").style.opacity = "0.7";
+            card.querySelector(".first__card").style.transition = "1s";
           });
 
           rotateCard = [];
@@ -97,7 +107,7 @@ function flipCard(clickedCard) {
         setTimeout(() => {
           rotateCard.forEach((card) => {
             card.classList.remove("flip", "disabled");
-            card.querySelector(".second__card").style.display = 'block';
+            card.querySelector(".second__card").style.display = "block";
             card.querySelector(".second__card").style.display = "flex";
             card.querySelector(".second__card").style.justifyContent = "center";
             card.querySelector(".second__card").style.alignItems = "center";
@@ -111,4 +121,3 @@ function flipCard(clickedCard) {
     }
   }
 }
-
